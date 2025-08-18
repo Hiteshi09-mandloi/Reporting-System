d56e0e2a5183e60c640cf110c0f99d9cdbacc046
Hiteshi09-mandloi <mandloihiteshi@gmail.com>
2025-08-18 14:50:30 +0530

    add url2

--- src/main/java/com/adtech/reportingsystem/config/WebConfig.java
+++ src/main/java/com/adtech/reportingsystem/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                // .allowedOrigins("http://localhost:3000","https://adtech-reporting-system-n1w9.vercel.app")
-                .allowedOrigins("https://adtech-reporting-system-n1w9.vercel.app\")")
+                .allowedOrigins("https://adtech-reporting-system-n1w9.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
