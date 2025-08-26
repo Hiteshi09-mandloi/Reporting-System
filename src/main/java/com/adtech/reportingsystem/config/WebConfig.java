c38c2c18d2137379f1da00ac262c8c2f7ef92abb
Hiteshi09-mandloi <mandloihiteshi@gmail.com>
2025-08-26 10:09:47 +0530

    add modify code webconfig

--- src/main/java/com/adtech/reportingsystem/config/WebConfig.java
+++ src/main/java/com/adtech/reportingsystem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000","https://adtech-reporting-system-n1w9.vercel.app")
+                .allowedOrigins("http://localhost:3000","https://ad-tech-reporting-system-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
